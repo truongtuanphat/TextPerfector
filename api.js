8986f7d8d4ce330baecf419b2d9bda0a46e5cc26
Phat Truong <truongtuanfat@gmail.com>
2023-02-06 00:22:40 +0700

    rename project

--- api.js
+++ api.js
@@ -1,29 +1,26 @@
-const openaiApiKey = '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';
+const openaiApiKey = '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';
 const model = 'text-davinci-edit-001';
-const prompt = 'Bec4me an d0ctor b3e hard';
+const prompt = 'Bec4me d0ctor b3e hard';
 
 async function getAnswer() {
-    console.log('Prompt: ' + prompt);
-    try {
-      const response = await fetch('https://api.openai.com/v1/edits', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + openaiApiKey
-        },
-        body: JSON.stringify({
-          model: model,
-          temperature: 0,
-          input: prompt,
-          instruction: "Fix the spelling mistakes, remove redundant characters and correct grammar"
-        })
-      });
-      console.log('OpenAI response received.');
-      const answer = await response.json();
-      console.log(answer.choices[0].text);
-    } catch (error) {
-      console.error(error);
-    }
+  console.log('Prompt: ' + prompt);
+  try {
+    const response = await fetch('https://api.openai.com/v1/edits', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + openaiApiKey
+      },
+      body: JSON.stringify({
+        model: model,
+        temperature: 0,
+        input: prompt,
+        instruction: "Fix the spelling mistakes, remove redundant characters and correct grammar"
+      })
+    });
+    const answer = await response.json();
+    console.log(answer.choices[0].text);
+  } catch (error) {
+    console.error(error);
   }
-
-getAnswer();
+}
--- event.js
+++ event.js
@@ -1,7 +1,7 @@
 var input = document.getElementById("input");
 
-input.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      document.getElementById("output").innerHTML = input.value;
-    }
-  });
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    document.getElementById("output").innerHTML = "result should be here";
+  }
+});
